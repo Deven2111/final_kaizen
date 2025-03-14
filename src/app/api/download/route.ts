1f02215e28853f52510a7607ce6735671de5d934
Deven2111 <devenrandhir9821@gmail.com>
2025-03-14 20:50:37 +0530

    Fix: Cliet Email Error Fix5

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { google } from 'googleapis';
 import { Readable } from 'stream';
-import { auth } from '@/lib/drive';
+import auth from '@/lib/drive';
 
 export async function GET(request: NextRequest) {
   try {
